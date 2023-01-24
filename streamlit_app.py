858eaf088b9fc37f54a8115ef1027eb9231ec77f
dpappsnowflake <123464457+dpappsnowflake@users.noreply.github.com>
2023-01-24 11:46:16 +0000

    Update streamlit_app.py
    Added defaults

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include
 # First Set the index to be fruit names (don't want numbers on the table)
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) # Defaults
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
