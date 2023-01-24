182eb74ab6ef2643a9b37d3da76fa95dada01ce6
dpappsnowflake <123464457+dpappsnowflake@users.noreply.github.com>
2023-01-24 16:31:57 +0000

    Update streamlit_app.py
    Adding fruityvice integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -22,3 +23,23 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 # Display the table on the page.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+########################################
+# Fruityvice integration
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# Normalise json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display fruityvice
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+
+
+
+
+
