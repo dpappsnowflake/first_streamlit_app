b39c844f8315977abd63f32c0200d11332ef5d1f
dpappsnowflake <123464457+dpappsnowflake@users.noreply.github.com>
2023-01-24 11:33:57 +0000

    Update streamlit_app.py
    Added data from bucket, displayed in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My parents' new healthy diner")
 streamlit.header("Breakfast menu")
@@ -8,3 +11,4 @@ streamlit.text("🐔    Hard-boiled free-range egg")
 streamlit.text("🥑🍞 Avocado toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
