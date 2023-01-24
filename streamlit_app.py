fa20a30e30f46abe9a47a451f1676191d2b8cc9f
dpappsnowflake <123464457+dpappsnowflake@users.noreply.github.com>
2023-01-24 17:15:27 +0000

    Update streamlit_app.py
    Testingsnowflake db selects

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ streamlit.dataframe(fruityvice_normalized)
 # Snowflake integration
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit Load list contains:")
 streamlit.text(my_data_row)
 
 
