16f6f6a91c29b4f722625801ece36238eea8a72c
dpappsnowflake <123464457+dpappsnowflake@users.noreply.github.com>
2023-01-24 17:43:36 +0000

    Update streamlit_app.py
    Changing fruityvice integartion to load on demand only

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -29,19 +30,28 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice integration
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information on')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-# Normalise json response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Display fruityvice
-streamlit.dataframe(fruityvice_normalized)
+    # Normalise json response
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # Display fruityvice
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 
 ########################################
 # Snowflake integration
+
+streamlit.stop()
+
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
@@ -50,6 +60,7 @@ streamlit.header("The fruit Load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
 streamlit.write("Thanks for adding " + add_my_fruit)
 
 
